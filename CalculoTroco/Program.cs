fea28f7e5bc1db0cd09332f4b89b231f01740060
Gabriela <gabriela.priaro@gmail.com>
2024-10-22 16:34:15 +0100

    Implementaçao para calculo de troco.

--- CalculoTroco/Program.cs
+++ CalculoTroco/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace CalculoTroco
+﻿//Problema "troco"
+//Fazer um programa para calcular o troco no processo de pagamento de um produto de uma mercearia.
+//O programa deve ler o preço unitário do produto, a quantidade de unidades compradas deste produto,
+//e o valor em dinheiro dado pelo cliente (suponha que haja dinheiro suficiente). Seu programa deve
+//mostrar o valor do troco a ser devolvido ao cliente. 
+
+using System.Globalization;
+
+namespace CalculoTroco
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            double precoProduto, dinheiroRecebido, trocoRecebido;
+            int quantidadeComprada;
+
+            CultureInfo CI = CultureInfo.InvariantCulture;
+ 
+            Console.Write("Preço unitário do produto: ");
+            precoProduto = double.Parse(Console.ReadLine(), CI);
+
+            Console.Write("Quantidade comprada: ");
+            quantidadeComprada = int.Parse(Console.ReadLine());
+
+            Console.Write("Dinheiro recebido: ");
+            dinheiroRecebido = double.Parse(Console.ReadLine(), CI);
+
+            trocoRecebido = dinheiroRecebido - precoProduto * quantidadeComprada;
+
+            Console.Write($"TROCO = {trocoRecebido.ToString("F2", CI)}");
         }
     }
 }
